b404ddd33df8a90d12185cd8c02a2c03a47e1d78
Raghunandan Vellur Prasanna <nandan.raghu002@gmail.com>
2022-06-28 21:08:17 -0400

    made-changes

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+//Routes
+const sAuth = require('./routes/seekerAuth');
+const rAuth = require('./routes/recruiterAuth');
 
 //Middlewares
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 
+//Routes Middlewares
+app.use('/api/joblogin', sAuth);
+app.use('/api/reclogin', rAuth);
+
+//connect to db
 mongoose.connect(process.env.DB_CONNECT, 
     () => console.log('connected to DB!')
 );
 
-app.listen(3001, () => console.log('Server up and running'));
+//Connect to server
+app.listen(3002, 
+    () => console.log('Server up and running')
+);
--- routes/seekerAuth.js
+++ routes/seekerAuth.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const users = require('../model/seekerModel');
+const users = require('../models/seekerModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { registerValidation, loginValidation } = require('../validate');
@@ -28,3 +28,20 @@ router.post('/register', async (req, res) => {
         res.status(400).send(err);
     }
 });
+
+//Login
+router.post('/login', async (req, res) => {
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const user = await users.findOne({email: req.body.email});
+    if(!user) return res.status(400).send("Email doesn't exists");
+    
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Email or Password is incorrect');
+    
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send({token});
+});
+
+module.exports = router;
